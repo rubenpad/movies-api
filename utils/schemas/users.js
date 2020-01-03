52396a7deee5cc6502254a52d9d3728aaf302d32
Ruben Padilla <rubbenpad2@gmail.com>
2020-01-02 22:54:25 -0500

    creating users service

---
+++ utils/schemas/users.js
@@ -0,0 +1,17 @@
+const Joi = require('@hapi/joi');
+
+const userIdSchema = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
+
+const createUserSchema = {
+  name: Joi.string()
+    .max(100)
+    .required(),
+  email: Joi.string().email(),
+  password: Joi.string().required(),
+  isAdmin: Joi.boolean
+};
+
+module.exports = {
+  userIdSchema,
+  createUserSchema
+};
